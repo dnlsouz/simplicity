783d8b8cc2970e030d9342a4547842024478a8b7
dnlsouz <daniel.checca@gmail.com>
2023-10-25 16:12:52 -0300

    Atualizações

--- js/contato.js
+++ js/contato.js
@@ -70,3 +70,35 @@ botaoBuscar.addEventListener("click", async function(event){
     campoEstado.value = dados.uf;
    }
 });
+
+
+/* programação do Formspree */
+ 
+async function handleSubmit(event) {
+   event.preventDefault();
+   var status = document.getElementById("my-form-status");
+   var data = new FormData(event.target);
+   fetch(event.target.action, {
+     method: formulario.method,
+     body: data,
+     headers: {
+         'Accept': 'application/json'
+     }
+   }).then(response => {
+     if (response.ok) {
+       status.innerHTML = "Obrigado por entrar em contato conosco, retornarememos em breve";
+       formulario.reset()
+     } else {
+       response.json().then(data => {
+         if (Object.hasOwn(data, 'errors')) {
+           status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+         } else {
+           status.innerHTML = "Percebemos um erro, tente novamente mais tarde"
+         }
+       })
+     }
+   }).catch(error => {
+     status.innerHTML = "Percebemos um erro, tente novamente mais tarde"
+   });
+ }
+ formulario.addEventListener("submit", handleSubmit)
